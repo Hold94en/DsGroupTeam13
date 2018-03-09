6c65ab9595cf6931aff45b0d14e071de3cddddc8
simonazzi joss <simonazzijoss@gmail.com>
2018-03-09 12:58:01 +0100

    Checkpoint

--- source/js/fetchLocation.js
+++ source/js/fetchLocation.js
@@ -1,6 +1,6 @@
 //jshint esversion: 6
 
-var getLocations = function(successClb, errorClb) {
+const getLocations = function(successClb, errorClb) {
 	const obj = {
 		locations:[
 			{
@@ -20,12 +20,71 @@ var getLocations = function(successClb, errorClb) {
 				],
 				description: "",
 				rewiews: [
-					"Lorem ipsum",
-					"Lorem ipsum"
-				]
+					{
+						title: "Titolo recensione 1",
+						text: "Lorem ipsum"
+					},
+					{
+						title: "Titolo recensione 2",
+						text: "Lorem ipsum"
+					}
+				],
+				rating: 5
+			},
+			{
+				id: "2",
+				name: "Ristorante 2",
+				address: {
+					state: "Italia",
+					city: "Milano",
+					street: "Via Roma",
+					strnum: "11",
+					postcode: "000000"
+				},
+				gallery: [
+					"../img/",
+					"../img/",
+					"../img/"
+				],
+				description: "",
+				rewiews: [
+					{
+						title: "Titolo recensione 3",
+						text: "Lorem ipsum"
+					}
+				],
+				rating: 3
+			},
+			{
+				id: "3",
+				name: "Ristorante 3",
+				address: {
+					state: "Italia",
+					city: "Torino",
+					street: "Via ###",
+					strnum: "999",
+					postcode: "111111"
+				},
+				gallery: [
+					"../img/",
+					"../img/",
+					"../img/"
+				],
+				description: "",
+				rewiews: [
+					{
+						title: "Titolo recensione 4",
+						text: "Lorem ipsum"
+					},
+					{
+						title: "Titolo recensione 5",
+						text: "Lorem ipsum"
+					}
+				],
+				rating: 1
 			}
 		]
 	};
-
+	
 	successClb(obj.locations);
 };
